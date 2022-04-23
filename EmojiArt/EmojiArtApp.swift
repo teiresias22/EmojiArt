4a16a6aee74625742183f86333fdd8e8210ead1e
Teiresias22 <teiresias1987@gmail.com>
2022-04-24 03:44:35 +0900

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Joonhwan Jeon on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
