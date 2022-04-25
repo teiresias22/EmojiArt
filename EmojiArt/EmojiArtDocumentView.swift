a4159bec9202eb2ebb5b960d4e8ced7f0d4edebd
Teiresias22 <teiresias1987@gmail.com>
2022-04-25 23:31:55 +0900

    Day9. Drag and Drop

---
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,140 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Joonhwan Jeon on 2022/04/24.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    
+    let defaultEmojiFontSize: CGFloat = 40
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            documentBody
+            palette
+        }
+    }
+    
+    var documentBody: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color.yellow
+                ForEach(document.emojis) { emoji in
+                    Text(emoji.text)
+                        .font(.system(size: fontSize(for: emoji)))
+                        .position(position(for: emoji, in: geometry))
+                }
+            }
+            .onDrop(of: [.plainText], isTargeted: nil) { prividers, location in
+                return false
+            }
+        }
+    }
+    
+    private func position(for emoji: EmojiArtModel.Emoji, in geometry: GeometryProxy) -> CGPoint {
+        convertFromEmojiCoordinates((emoji.x, emoji.y), in: geometry)
+    }
+    
+    private func convertFromEmojiCoordinates(_ location: (x: Int, y: Int), in geometry: GeometryProxy) -> CGPoint {
+        let center = geometry.frame(in: .local).center
+        return CGPoint(
+            x: center.x + CGFloat(location.x),
+            y: center.y + CGFloat(location.y)
+        )
+    }
+    
+    private func fontSize(for emoji: EmojiArtModel.Emoji) -> CGFloat {
+        CGFloat(emoji.size)
+    }
+    
+    
+    var palette: some View {
+        ScrollingEmojisView(emojis: testEmojis)
+            .font(.system(size: defaultEmojiFontSize))
+    }
+    
+    let testEmojis = "😀💡🖥🤵‍♂️📋😁😄🙋😃⚒📞👨‍💻💁📄😱🎉📱👨‍🏫😺📪🗺🤔🙈"
+    
+}
+
+struct ScrollingEmojisView: View {
+    let emojis: String
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis.map { String($0) }, id: \.self) { emoji in
+                    Text(emoji)
+                        .onDrag { NSItemProvider(object: emoji as NSString) }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiArtDocumentView(document: EmojiArtDocument())
+    }
+}
---
+++ EmojiArt/EmojiArtModel.swift
@@ -0,0 +1,39 @@
+//
+//  EmojiArtModel.swift
+//  EmojiArt
+//
+//  Created by Joonhwan Jeon on 2022/04/24.
+//
+
+import Foundation
+
+struct EmojiArtModel {
+    var background = Background.blank
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable, Hashable {
+        let text: String
+        var x: Int // offset from the center
+        var y: Int // offset from the center
+        var size: Int
+        var id: Int
+        
+        //private를 선언하기 위해서 작성함
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int){
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    init() { }
+    
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(_ text: String, at location: (x: Int, y: Int), size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: location.x, y:location.y, size: size, id: uniqueEmojiId))
+    }
+}
